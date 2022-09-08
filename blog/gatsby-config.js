133041257e467974b8c432879bcd0f047e0e5835
brenden hunter <hunterb7@gmatc.matc.edu>
2022-09-08 14:45:24 -0500

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,9 +1,14 @@
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog.`,
+    author: `Brenden Hunter`,
+    contact:{
+        name: 'Brenden Hunter',
+        company: 'Blogs Inc.',
+        address: 'PO Box 1234'
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -31,4 +36,4 @@ module.exports = {
       },
     },
   ],
-}
+};
