bf083ff62041da077c13695e0d5f84a2729bb381
brenden hunter <hunterb7@gmatc.matc.edu>
2022-11-27 11:36:58 -0600

    Completed Activity 9

--- blog/src/components/Themes/Gray/Gray.js
+++ blog/src/components/Themes/Gray/Gray.js
@@ -1,5 +1,17 @@
+import React from 'react'
+import mainHeaderImage from './images/Assets/react.png'
+import { Search } from 'styled-components/feather'
+
+const images = {
+    mainHeaderImage
+}
+
+const icons = {
+    Search: <Search />
+}
+
 const theme = {
     header: { backgroundColor: '#555' }
 }
 
-export { theme as Gray }
+export const Gray = { ...theme, variants, images, icons }
